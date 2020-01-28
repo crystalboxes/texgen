e35bf4d386bbe913709ae8f11b6e932a47f0e759
crystalboxes <ursus95@gmail.com>
2020-01-28 14:25:16 +0200

    rewrite color class

--- src/Core/Color.js
+++ src/Core/Color.js
@@ -1,39 +1,32 @@
 import { ColorComponent } from '../components/ColorComponent.jsx'
-import {Displayable} from '../core/Displayable.js'
+import { Displayable } from '../core/Displayable.js'
 
 let divisor = 1 / 255.0
 // range 0..255
 export class Color extends Displayable {
-  constructor(r, g, b) {
-    super()
-    this.params = {
+  isFloat = false
+  static make(r, g, b) {
+    let c = new Color
+    c.params = {
       r: { value: r | 0, rangeMin: 0, rangeMax: 255 },
       g: { value: g | 0, rangeMin: 0, rangeMax: 255 },
       b: { value: b | 0, rangeMin: 0, rangeMax: 255 }
     }
+    return c
   }
+
+  static makeFloat(r, g, b, a) {
+    let c = new Color
+    c.isFloat = true
+    this.params = { r: r, g: g, b: b, a: a }
+  }
+
   display = ColorComponent
 
-  get r() { return this.params.r.value * divisor }
-  get g() { return this.params.g.value * divisor }
-  get b() { return this.params.b.value * divisor }
-  get a() { return 1.0 }
+  get r() { return this.isFloat ? this.params.r : this.params.r.value * divisor }
+  get g() { return this.isFloat ? this.params.g : this.params.g.value * divisor }
+  get b() { return this.isFloat ? this.params.b : this.params.b.value * divisor }
+  get a() { return this.isFloat ? this.params.a : 1.0 }
+
 }
 export default Color
-// // range 0..1
-// export class ColorRGBA extends Displayable {
-//   constructor(r, g, b, a) {
-//     super()
-//     this.params = {r:r,g:g,b:b,a:a}
-//   }
-//   get r() { return this.params.r }
-//   get g() { return this.params.g }
-//   get b() { return this.params.b }
-//   get a() { return this.params.a }
-
-//   get rgba() {
-//     return this
-//   }
-// }
-
-// export default {Color, ColorRGBA}
