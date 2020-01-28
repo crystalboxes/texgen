f19541cb69caeae7b89a4f801d7b0dc73f1388ef
crystalboxes <ursus95@gmail.com>
2020-01-28 14:58:10 +0200

    add css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import {TexGen} from "./behaviors/TexGen.js"
 import App from './core/App.js'
 import { AppComponent } from './components/AppComponent.jsx'
+import './style.css'
 
 let app = new App(new TexGen, {width: 800, height: 600})
 ReactDOM.render(<AppComponent instance={app} />, document.getElementById("main"))
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
         }
       },
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.glsl$/,
         loader: 'webpack-glsl-loader'
       },
