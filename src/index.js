888cde1e09a20723fe954c0fa8698f3ef260da0c
crystalboxes <ursus95@gmail.com>
2020-01-29 19:57:45 +0200

    add sass

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 import {TexGen} from "./behaviors/TexGen.js"
 import App from './core/App.js'
 import { AppComponent } from './components/AppComponent.jsx'
-import './style.css'
+import './style.scss'
 
 let app = new App(new TexGen, {width: 800, height: 800})
 ReactDOM.render(<AppComponent instance={app} />, document.getElementById("main"))
